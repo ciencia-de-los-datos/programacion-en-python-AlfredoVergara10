086713a860a28f57db89752aff355d57f49c3691
AlfredoVergara10 <112211579+AlfredoVergara10@users.noreply.github.com>
2022-10-18 19:19:43 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -25,8 +25,10 @@ def pregunta_01():
 data = open ('data.csv', 'r').readlines()
 data = [row.replace('\n', '') for row in data]
 data = [row.split('\t') for row in data]
+
 summatory = 0
 for row in data: summatory = summatory + int(row[1]) 
+    
 print (summatory)
     
     return 
